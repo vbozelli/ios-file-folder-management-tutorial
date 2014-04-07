a13c3b6e2b413e42db1a2ed5018bfc5ad7ccad21
vbozelli <vbozelli@hotmail.com>
2014-04-07 16:52:26 -0300

    Testando commit

---
+++ Receitas/Receitas/Ingrediente.h
@@ -0,0 +1,13 @@
+//
+//  Ingrediente.h
+//  Receitas
+//
+//  Created by Victor Bozelli Alvarez on 07/04/14.
+//  Copyright (c) 2014 Vinicius Miana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ingrediente : NSObject
+
+@end
---
+++ Receitas/Receitas/Ingrediente.m
@@ -0,0 +1,13 @@
+//
+//  Ingrediente.m
+//  Receitas
+//
+//  Created by Victor Bozelli Alvarez on 07/04/14.
+//  Copyright (c) 2014 Vinicius Miana. All rights reserved.
+//
+
+#import "Ingrediente.h"
+
+@implementation Ingrediente
+
+@end
